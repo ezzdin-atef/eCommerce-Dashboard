943a723312323734e49a755a02cc5fad256b5f3f
Ezzdin Atef <ezzdin1125@gmail.com>
2020-10-27 11:03:23 +0200

    update v1.1.1

--- src/pages/WhatIsNew.jsx
+++ src/pages/WhatIsNew.jsx
@@ -8,12 +8,21 @@ const WhatIsNew = () => {
       </header>
       <div className="content">
         <div className="version">
+          <h4>v1.1.1 <span>(Updated on: 27/10/2020)</span></h4>
+          <p>
+            <ul>
+              <li><span className="fix">Fix</span>Fix the padding and margin problems</li>
+              <li><span className="improve">Improve</span>Add budget to the what's new page</li>
+            </ul>
+          </p>
+        </div>
+        <div className="version">
           <h4>v1.1.0 <span>(Updated on: 23/10/2020)</span></h4>
           <p>
             <ul>
-              <li>Add "What's New?" page</li>
-              <li>Add The Alter Div in products and wishlist pages</li>
-              <li>Make the delete smooth in wishlist and products pages</li>
+              <li><span className="feature">Feature</span>Add "What's New?" page</li>
+              <li><span className="improve">Improve</span>Add The Alter Div in products and wishlist pages</li>
+              <li><span className="improve">Improve</span>Make the delete smooth in wishlist and products pages</li>
             </ul>
           </p>
         </div>
@@ -21,12 +30,12 @@ const WhatIsNew = () => {
           <h4>v1.0.0 <span>(Updated on: 21/10/2020)</span></h4>
           <p>
             <ul>
-              <li>Finish The Orders Page</li>
-              <li>Finish The Products Page</li>
-              <li>Finish The WishLlist Page</li>
-              <li>Finish The Orders Page</li>
-              <li>Finish the implementation of redux with all the finished pages</li>
-              <li>make the dropdown and the popups smooth</li>
+              <li><span className="feature">Feature</span>Finish The Orders Page</li>
+              <li><span className="feature">Feature</span>Finish The Products Page</li>
+              <li><span className="feature">Feature</span>Finish The WishLlist Page</li>
+              <li><span className="feature">Feature</span>Finish The Orders Page</li>
+              <li><span className="improve">Improve</span>Finish the implementation of redux with all the finished pages</li>
+              <li><span className="improve">Improve</span>make the dropdown and the popups smooth</li>
             </ul>
           </p>
         </div>
